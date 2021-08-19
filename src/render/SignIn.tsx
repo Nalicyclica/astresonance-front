dfd28085dcaf7018c2f77fae962eeb34bdd822d2
Nalicyclica <siruri0xf3@gmail.com>
2021-08-19 21:56:25 +0900

    axiosでpostを使いログイン情報が取得できることの確認

--- src/render/SignIn.tsx
+++ src/render/SignIn.tsx
@@ -1,24 +1,41 @@
 import React, { useContext } from 'react';
+import axios from 'axios';
 import { CurrentUser, UserInfo, defaultUserInfo } from './Main';
 import { useForm } from "react-hook-form"
+
+type inputInfo = {
+  email: string
+  password: string
+};
 
 const SignIn: React.FC = () => {
   const { userInfo, setUserInfo } = useContext(CurrentUser)
   const { register, handleSubmit, watch, formState: {errors} } = useForm();
-  const onSubmit = (data: UserInfo) => {
-    const userData: UserInfo = {...defaultUserInfo, ...data};
-    console.log("prev:")
-    console.log(userInfo)
-    setUserInfo(userData)
-    console.log("cons:")
-    console.log(userInfo)
+  const onSubmit = (data: inputInfo) => {
+    const axiosConfig = axios.create({
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Headers': 'access-token',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+      },
+      responseType: 'json'
+    });
+    axiosConfig.post(
+      'http://localhost:3000/auth/sign_in',
+      data
+      ).then((response) => {
+        console.log(response.headers);
+        console.log(response);
+      });
+      // const userData: UserInfo = {...defaultUserInfo, ...data};
   };
 	return (
     <form onSubmit={handleSubmit(onSubmit)} className="flex-grow flex flex-col justify-start items-center w-screen text-gray-100">
       <h1 className="text-2xl mt-8 mb-6 px-4 text-yellow-300 border-b border-yellow-300">Sign in with your nickname</h1>
       <label className="my-2">
         <p>E-mail:</p>
-        <input type="text" {...register("uid")} placeholder="Enter your nickname" onChange={hoge => hoge} className="my-2 p-2 bg-gray-300 focus:bg-gray-100 focus:outline-none focus:shadow-bright rounded-md text-black"/>
+        <input type="text" {...register("email")} placeholder="Enter your nickname" onChange={hoge => hoge} className="my-2 p-2 bg-gray-300 focus:bg-gray-100 focus:outline-none focus:shadow-bright rounded-md text-black"/>
       </label>
       <label className="my-2">
         <p>Password:</p>
