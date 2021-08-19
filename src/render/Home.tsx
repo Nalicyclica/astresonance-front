6e8f7f04e2a862051434ae6d9932aab8190ee758
Nalicyclica <siruri0xf3@gmail.com>
2021-08-19 15:28:47 +0900

    portの変更とaxiosレスポンスの確認

--- src/render/Home.tsx
+++ src/render/Home.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import axios from 'axios';
 import {FaGooglePlay} from 'react-icons/fa'
 
 const Home: React.FC = () => {
+  const handlerClick = () => {
+    axios.get("http://localhost:3000/musics")
+    .then(response => console.log(response))
+  };
 	return (
     <div className="flex-grow flex flex-col justify-start items-center w-screen">
       <div className="w-96 max-w-full h-16 mx-8 my-6">
@@ -10,7 +15,7 @@ const Home: React.FC = () => {
           <div className="">Category: World Music</div>
         </div>
       </div>
-      <div className="shadow-bright hover:shadow-gold w-96 h-96 rounded-full align-middle flex justify-center items-center">
+      <div className="shadow-bright hover:shadow-gold w-96 h-96 rounded-full align-middle flex justify-center items-center" onClick={handlerClick}>
         <FaGooglePlay size={100} color={'#ccc'} />
       </div>
     </div>
