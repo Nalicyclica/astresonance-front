15991a25808c80820676fe2da5d83e87942537c6
Nalicyclica <siruri0xf3@gmail.com>
2021-08-23 21:48:57 +0900

    自分がタイトルをつけているとタイトルと他の人のタイトルリストが表示される機能を実装

--- src/render/MusicShow.tsx
+++ src/render/MusicShow.tsx
@@ -6,9 +6,39 @@ import { CurrentUser } from "./Main";
 import { musicInfo, getGenreName, getCategoryName } from "./Home";
 import { authToken, getAuth } from "../functions/Auth"
 
-type titleInfo = {
+type titleInput = {
   title: string
   color: string
+};
+
+type titleInfo = {
+  id: number
+  title: string
+  color: string
+  user_id: number
+  music_id: number
+  nickname: string
+  icon_color: string
+};
+
+type userTitleInfo = {
+  titleData: titleInfo
+  isTitled: boolean
+};
+
+const  defaultTitleInfo: titleInfo = {
+  id: 0,
+  title: "",
+  color: "",
+  user_id: 0,
+  music_id: 0,
+  nickname: "",
+  icon_color: ""
+};
+
+const defaultUserTitleInfo: userTitleInfo = {
+  titleData: defaultTitleInfo,
+  isTitled: false
 };
 
 const defaultMusicInfo: musicInfo = {
@@ -19,13 +49,22 @@ const defaultMusicInfo: musicInfo = {
   categoryName: "", 
   music_url: "",
   user_id: 1
-}
+};
+
+const MusicTitled: React.FC<{userTitle: titleInfo}> = props => {
+  return(
+    <div className="flex flex-col items-center w-96 h-16 mx-8 my-6 rounded-md text-gray-100 px-3 py-1 shadow-bright">
+      <div className="">あなたがつけたタイトル:</div>
+      <p>{props.userTitle.title}</p>
+    </div>
+  )
+};
 
 const MakeTitleForSignedIn: React.FC<{currentMusicId: string}> = props => {
   const [responseErrors, setErrors] = useState<object>({});
   const history = useHistory();
   const { register, handleSubmit, watch, formState: {errors} } = useForm();
-  const postTitle = async (musicId: string, inputInfo: titleInfo) => {
+  const postTitle = async (musicId: string, inputInfo: titleInput) => {
     const currentAuth: authToken = getAuth();
     try {
       const response = await axios.post(
@@ -33,16 +72,17 @@ const MakeTitleForSignedIn: React.FC<{currentMusicId: string}> = props => {
         {title: inputInfo},
         {headers: currentAuth}
       );
+      console.log(inputInfo);
     } catch (error) {
       setErrors(error);
       console.log(error);
     };
   };
-  const onSubmit = (data: titleInfo) => {
+  const onSubmit = (data: titleInput) => {
         setErrors({});
         postTitle(props.currentMusicId, data);
         if(true){
-          history.goBack();
+          console.log("true");
         }else{
           console.log(responseErrors);
         }
@@ -78,18 +118,28 @@ const RejectTitleForSignOut: React.FC = () => {
 
 const MusicShow: React.FC = () => {
   const [ currentMusic, setMusic ] = useState<musicInfo>(defaultMusicInfo);
+  const [ musicTitles, setmusicTitles ] = useState<titleInfo[]>([]);
+  const [ currentUserTitle, setUserTitle ] = useState<userTitleInfo>(defaultUserTitleInfo);
   const { userInfo, setUserInfo} = useContext(CurrentUser);
   const {id: currentMusicId} = useParams<{id: string}>();
   const fetchMusic = async (musicId: string) => {
     const musicShowUrl = `http://localhost:3000/musics/${musicId}`
-    console.log(musicShowUrl);
+    const currentAuth: authToken = getAuth();
+    const userTitleData: userTitleInfo = {...defaultUserTitleInfo};
     try{
-      const response = await axios.get(musicShowUrl);
-      const data: musicInfo = response.data;
+      const response = await axios.get(musicShowUrl,{headers: currentAuth});
+      const data: musicInfo = {...response.data};
       data.genreName = getGenreName(data.genre_id);
       data.categoryName = getCategoryName(data.category_id);
-      setMusic(data);
-    } catch(errors){
+      setMusic({...data});
+      if(response.data.user_title){
+        userTitleData.titleData = {...response.data.user_title};
+        userTitleData.isTitled = true;
+        const musicTitlesData: titleInfo[] = response.data.titles;
+        setmusicTitles(musicTitlesData);
+      }
+      setUserTitle(userTitleData);
+      } catch(errors){
       console.log(errors);
     };
   };
@@ -97,15 +147,41 @@ const MusicShow: React.FC = () => {
     fetchMusic(currentMusicId);
   },[]);
 
+  const titleList = musicTitles.map((titleItem) =>
+  <li key={titleItem.id} className="bg-gray-800 p-2 mb-1 w-64 rounded-md shadow-bright hover:shadow-gold hover:bg-gray-600 text-gray-100">
+    <Link to={`/Titles/${titleItem.id}`} className="flex justify-start">
+      <div style={{backgroundColor: titleItem.icon_color}} className = "w-8 h-8 rounded-full shadow-bright">
+      </div>
+      <div className ="flex justify-between items-center w-full pr-6">
+        <p>{titleItem.title}</p>
+        <p className="text-sm">by {titleItem.nickname}</p>
+      </div>
+    </Link>
+  </li>
+  );
+
   return (
     <div>
-      <div className= "flex flex-col justify-between items-center w-screen h-home">
-        { userInfo.isSignIn? <MakeTitleForSignedIn currentMusicId={currentMusicId} /> : <RejectTitleForSignOut />}
-        <div className="my-8">
-          <audio controls src={currentMusic.music_url}/>
+      <div className="flex justify-between">
+        <div className= "flex flex-col justify-between items-center w-screen h-home">
+          { userInfo.isSignIn? ( currentUserTitle.isTitled? <MusicTitled userTitle = {currentUserTitle.titleData}/> : <MakeTitleForSignedIn currentMusicId={currentMusicId}/>) : <RejectTitleForSignOut />}
+          <div className="my-8">
+            <audio controls src={currentMusic.music_url}/>
+          </div>
+        </div>
+        <div>
+
+        { userInfo.isSignIn && currentUserTitle.isTitled &&
+          <div>
+            <p className="text-lg">他の人がつけたタイトル</p>
+            <ul className="overflow-y-auto p-4 h-96 m-12">
+              { titleList }
+            </ul>
+          </div>
+        }
         </div>
       </div>
-      <div className= "flex justify-between items-center p-1">
+      <div className= "flex justify-between items-center p-1 h-20">
         <div className="mx-4">
           ジャンル:{currentMusic.genreName}の{currentMusic.categoryName}
         </div>
