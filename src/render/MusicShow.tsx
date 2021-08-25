154499e5bf00239b8ffe5c7ce8f534331a5b8790
Nalicyclica <siruri0xf3@gmail.com>
2021-08-25 12:03:04 +0900

    タイトルをクリックするとタイトルを個別表示をする欄が生成する機能を実装

--- src/render/MusicShow.tsx
+++ src/render/MusicShow.tsx
@@ -11,6 +11,11 @@ type titleInput = {
   color: string
 };
 
+type currentShow = {
+  showFlag: boolean
+  showId: number
+}
+
 type titleInfo = {
   id: number
   title: string
@@ -24,6 +29,11 @@ type titleInfo = {
 type userTitleInfo = {
   titleData: titleInfo
   isTitled: boolean
+};
+
+const defaultShow: currentShow = {
+  showFlag: false,
+  showId: -1
 };
 
 const  defaultTitleInfo: titleInfo = {
@@ -123,12 +133,39 @@ const YourPostedMusic: React.FC = () => {
   );
 };
 
+const TitleItem: React.FC<{titleItem: titleInfo, setTitleShow: (value: currentShow) => void}> = props => {
+  const thisTitleShow = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    const setCurrent: currentShow = {
+      showFlag: true,
+      showId: props.titleItem.id,
+    };
+    props.setTitleShow(setCurrent);
+  };
+
+  return(
+    <li key={props.titleItem.id} className="bg-gray-800 p-2 mb-1 w-64 rounded-md shadow-bright hover:shadow-gold hover:bg-gray-600 text-gray-100">
+    <button onClick={(e) => thisTitleShow(e)} className="flex justify-start">
+      <div style={{backgroundColor: props.titleItem.icon_color}} className = "w-8 h-8 rounded-full shadow-bright">
+      </div>
+      <div style={{color: props.titleItem.color}} className ="flex justify-between items-center w-full pr-6 text-shadow-ar">
+        <p>{props.titleItem.title}</p>
+        <p className="text-sm">by {props.titleItem.nickname}</p>
+      </div>
+    </button>
+  </li>
+  );
+};
+
+
 const MusicShow: React.FC = () => {
   const [ currentMusic, setMusic ] = useState<musicInfo>(defaultMusicInfo);
   const [ musicTitles, setmusicTitles ] = useState<titleInfo[]>([]);
   const [ currentUserTitle, setUserTitle ] = useState<userTitleInfo>(defaultUserTitleInfo);
+  const [ currentTitleShow, setTitleShow ] = useState<currentShow>(defaultShow);
   const { userInfo, setUserInfo} = useContext(CurrentUser);
   const {id: currentMusicId} = useParams<{id: string}>();
+
   const fetchMusic = async (musicId: string) => {
     const musicShowUrl = `http://localhost:3000/musics/${musicId}`
     const currentAuth: authToken = getAuth();
@@ -156,17 +193,12 @@ const MusicShow: React.FC = () => {
     fetchMusic(currentMusicId);
   },[]);
 
-  const titleList = musicTitles.map((titleItem) =>
-  <li key={titleItem.id} className="bg-gray-800 p-2 mb-1 w-64 rounded-md shadow-bright hover:shadow-gold hover:bg-gray-600 text-gray-100">
-    <Link to={`/Titles/${titleItem.id}`} className="flex justify-start">
-      <div style={{backgroundColor: titleItem.icon_color}} className = "w-8 h-8 rounded-full shadow-bright">
-      </div>
-      <div style={{color: titleItem.color}} className ="flex justify-between items-center w-full pr-6 text-shadow-ar">
-        <p>{titleItem.title}</p>
-        <p className="text-sm">by {titleItem.nickname}</p>
-      </div>
-    </Link>
-  </li>
+  useEffect(()=>{
+    console.log(currentTitleShow);
+  },[currentTitleShow]);
+
+  const titleList = musicTitles.map((titleItem) => 
+    <TitleItem titleItem={titleItem} setTitleShow={setTitleShow} />
   );
 
   return (
@@ -178,17 +210,20 @@ const MusicShow: React.FC = () => {
             <audio controls src={currentMusic.music_url}/>
           </div>
         </div>
-        <div>
 
         { userInfo.isSignIn && ( currentMusic.user_id == userInfo.id || currentUserTitle.isTitled ) &&
-          <div>
+          <div className="w-96 bg-gray-300">
             <p className="text-lg">他の人がつけたタイトル</p>
             <ul className="overflow-y-auto p-4 h-96 m-12">
               { titleList }
             </ul>
           </div>
         }
-        </div>
+        { currentTitleShow.showFlag &&
+          <div className="w-96 bg-gray-600">
+            <p className="text-lg">タイトル個別ページ欄 {currentTitleShow.showId}</p>
+          </div>
+        }
       </div>
       <div className= "flex justify-between items-center p-1 h-20">
         <div className="mx-4">
