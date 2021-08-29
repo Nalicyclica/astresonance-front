4e353b89be7ea74c72dc66fed5074bf1514573f7
Nalicyclica <siruri0xf3@gmail.com>
2021-08-29 21:26:17 +0900

    タイトルとコメントの削除機能を実装

---
+++ src/functions/DeleteComment.ts
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import axios from "axios";
+import { authToken, getAuth } from "./Auth";
+import { responseInfo, defaultResponseInfo } from "./DeleteMusic";
+
+export const useCommentDelete = () => {
+  const [responseState, setResponseInfo] = useState<responseInfo>(defaultResponseInfo);
+  const commentDelete = async (commentId: number) => {
+    const currentAuth: authToken = getAuth();
+    const url: string = `${process.env.REACT_APP_SERVER_DOMAIN}/comments/${commentId}`
+    try {
+      const response = await axios.delete(
+        url,
+        { headers: currentAuth}
+      );
+      const responseData: responseInfo = {
+        valid: true,
+        id: commentId,
+        errors: {}
+      };
+      setResponseInfo(responseData);
+    } catch (error){
+      const responseData: responseInfo = {
+        valid: false,
+        id: commentId,
+        errors: {}
+      };
+      setResponseInfo(responseData);
+    };
+  };
+  return [responseState, commentDelete] as const;
+};
---
+++ src/functions/DeleteTitle.ts
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import axios from "axios";
+import { authToken, getAuth } from "./Auth";
+import { responseInfo, defaultResponseInfo } from "./DeleteMusic";
+
+export const useTitleDelete = () => {
+  const [responseState, setResponseInfo] = useState<responseInfo>(defaultResponseInfo);
+  const titleDelete = async (titleId: number) => {
+    const currentAuth: authToken = getAuth();
+    const url: string = `${process.env.REACT_APP_SERVER_DOMAIN}/titles/${titleId}`
+    try {
+      const response = await axios.delete(
+        url,
+        { headers: currentAuth}
+      );
+      const responseData: responseInfo = {
+        valid: true,
+        id: titleId,
+        errors: {}
+      };
+      setResponseInfo(responseData);
+    } catch (error){
+      const responseData: responseInfo = {
+        valid: false,
+        id: titleId,
+        errors: {}
+      };
+      setResponseInfo(responseData);
+    };
+  };
+  return [responseState, titleDelete] as const;
+};
