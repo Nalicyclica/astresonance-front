897f760b65b352d5a0476832e856552cb5361015
Nalicyclica <siruri0xf3@gmail.com>
2021-09-04 21:27:09 +0900

    ドロップダウンメニューの修正

--- src/render/Header.tsx
+++ src/render/Header.tsx
@@ -26,7 +26,7 @@ const Header: React.FC = () => {
   const logo_url = "./img/logo.png"
   const { userInfo, setUserInfo } = useContext(CurrentUser);
 	return (
-    <header className="h-20 sticky top-0 flex justify-between items-center backdrop-filter backdrop-blur-lg backdrop-contrast-75 shadow-header">
+    <header className="z-50 h-20 sticky top-0 flex justify-between items-center backdrop-filter backdrop-blur-lg backdrop-contrast-75 shadow-header">
       <Link to="/">
         <img src={logo_url} alt="AstResonance" className="h-16 m-2"></img>
       </Link>
--- src/render/SignOut.tsx
+++ src/render/SignOut.tsx
@@ -1,14 +1,12 @@
-import React, {useEffect} from "react"
-import { useHistory } from "react-router-dom";
+import React from "react"
 import { useContext } from "react";
 import { CurrentUser } from "../functions/UserInfo";
 
 const SignOut: React.FC = () => {
-  const history = useHistory();
   const {userInfo, setUserInfo} = useContext(CurrentUser);
 
   return(
-      <button onClick={setUserInfo.signOut} className="w-full h-full text-left">Sign out</button>
+      <button onClick={setUserInfo.signOut} className="w-full h-full text-center">サインアウト</button>
   );
 };
 
