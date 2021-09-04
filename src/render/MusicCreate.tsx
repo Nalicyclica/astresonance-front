367c0595d6c9f64ab26c62973f2a263b494dc84d
Nalicyclica <siruri0xf3@gmail.com>
2021-09-04 23:02:48 +0900

    音楽投稿ページの修正

--- src/render/MusicCreate.tsx
+++ src/render/MusicCreate.tsx
@@ -51,31 +51,31 @@ const MusicCreate: React.FC = () => {
   }, [responseState]);
   
 	return (
-    <div className="flex justify-center h-main overflow-auto">
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col justify-start items-center p-12 w-120 rounded-lg backdrop-filter backdrop-blur-md">
-        <h1 className="m-4 text-xl">音楽を投稿してください</h1>
+    <div className="flex justify-center">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col justify-start items-center p-6 w-120 text-gray-100 mb-12">
+        <h1 className="text-2xl mt-8 mb-2 px-4 text-yellow-400 border-b border-yellow-400">音楽を投稿してください</h1>
         {responseState.errors.errors && <p className="text-red-600">音楽を投稿できませんでした</p>}
-        <div className="mb-4">
+        <div className="mt-6 mb-4">
           <div {...getRootProps()} className="flex justify-center items-center w-96 h-48 bg-gray-300 text-black rounded-md shadow-bright hover:shadow-gold hover:bg-gray-100">
           <input {...getInputProps()} />
           { selectedMusic.name == ""? <p>Drag 'n' drop a mp3/ogg files</p> : <p>{selectedMusic.name}を投稿</p>}
           </div>
         </div>
         <label className="my-2">
-          <p>曲/歌:</p>
-          <select {...register("category_id")} className="my-2 p-2 bg-gray-900 border-b border-yellow-300 focus:outline-none hover:bg-gray-700">
+          <p className="text-shadow-black">曲/歌:</p>
+          <select {...register("category_id")} className="my-2 p-2 border-b border-gray-100 focus:outline-none hover:bg-gray-700 bg-transparent backdrop-filter backdrop-blur-lg">
             <option hidden>音楽の種類を選択してください</option>
             {categoryItems}
           </select>
         </label>
         <label className="my-2">
-          <p>ジャンル:</p>
-          <select {...register("genre_id")} className="my-2 p-2  bg-gray-900 border-b border-yellow-300 focus:outline-none hover:bg-gray-700">
+          <p className="text-shadow-black">ジャンル:</p>
+          <select {...register("genre_id")} className="my-2 p-2 border-b border-gray-100 focus:outline-none hover:bg-gray-700 bg-transparent backdrop-filter backdrop-blur-lg">
             <option hidden>音楽の種類を選択してください</option>
             {genreItems}
           </select>
         </label>
-        <input type="submit" value="音楽を投稿する" className="text-xl my-4 px-5 py-3 bg-gray-900 rounded-md shadow-bright hover:shadow-gold"/>
+        <input type="submit" value="音楽を投稿する" className="text-xl my-4 px-5 py-3 bg-gray-900 rounded-md text-yellow-400 shadow-bright hover:shadow-gold"/>
       </form>
     </div>
 	);
