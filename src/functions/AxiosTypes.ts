a17bf23d73b0ab15e3363915bc15364f8518eccd
Nalicyclica <siruri0xf3@gmail.com>
2021-09-10 14:48:39 +0900

    音楽一覧機能への実装

---
+++ src/functions/AxiosTypes.ts
@@ -0,0 +1,50 @@
+type ResponseResultInfo = {
+  valid: boolean
+  action: string
+  errors: any
+};
+
+export type ResponseInfo = {
+  loading: boolean
+  result: ResponseResultInfo
+};
+
+const defaultResponseResult: ResponseResultInfo = {
+  valid: false,
+  action: "",
+  errors: null
+};
+
+export const defaultResponse: ResponseInfo = {
+  loading: false,
+  result: {...defaultResponseResult}
+};
+
+export const loadingResponse: ResponseInfo = {
+  loading: true,
+  result: {...defaultResponseResult}
+};
+
+export const successResponse = (action: string): ResponseInfo => {
+  const successResponseData: ResponseInfo = {
+    loading: false,
+    result: {
+      valid: true,
+      action: action,
+      errors: null
+    }
+  };
+  return successResponseData;
+};
+
+export const errorResponse = (errors: any, action: string): ResponseInfo => {
+  const errorResponseData: ResponseInfo = {
+    loading: false,
+    result: {
+      valid: false,
+      action: action,
+      errors: errors
+    }
+  };
+  return errorResponseData;
+};
